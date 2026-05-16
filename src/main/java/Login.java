85d1482c27df19a37c52238ab0d64040277ceac6
Sachin7049 <127876681+Sachin7049@users.noreply.github.com>
2026-05-16 18:08:13 +0530

    Update Login.java

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -19,7 +19,7 @@ public class Login {
     public void login(String usernames,String Password) {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://preprod.parkengage.com/");
+        driver.get("");
         wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Login"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username"))).sendKeys(usernames);
         driver.findElement(By.id("pwd")).sendKeys(Password);
@@ -33,17 +33,11 @@ public class Login {
     public Object[][] set() {
 
         return new Object[][]{
-                {"superadmin@parkengage.com", "dfhef@yopmail"},
-        {"superadmin@1parkengage.com", "Welco]me@1234"},
-        {"superadmin@parkengage.com", "Pass9ord@123"}
+                {"sn@parkengage.com", "dfhef@yopmail"},
+        {"s999@1parkengage.com", "Welco]me@1234"},
+        {"sadmin999@parkengage.com", "Pass9ord@123"}
     };
-//         data[0][0]="superadmin@parkengage.com";
-//         data[0][1]="Superadmin@1234";
-//         data[1][0]="superadmin@1parkengage.com";
-//         data[1][1]="Welcome@1234";
-//         data[2][0]="superadmin@parkengage.com";
-//         data[2][1]="Password@123";
-
+//      
 
 
     }
